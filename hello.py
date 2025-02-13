507bc093de024883e5c546fe553bd7fc020471fa
Carl Berdin <berdincarlandre@gmail.com>
2025-02-13 15:32:37 +0800

    My first Python program

---
+++ hello.py
@@ -0,0 +1,9 @@
+Name = "Carl Berdin"
+Course = "BSIT"
+Interests_in_programming = "Programming"
+
+print("Hello world!")
+
+print("Name: " + Name)
+print("Course: " + Course)
+print("Interests in programming: " + Interests_in_programming) 
